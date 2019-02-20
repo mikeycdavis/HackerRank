d84e853504875f62c1f4cc79dd8d04d0aa1746f0
Michael Davis <mikeycdavis@gmail.com>
2019-02-19 19:07:26 -0800

    Solutions

---
+++ HackerRank/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Recursion.PascalTriangle(5);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HackerRank/Recursion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank
+{
+    public static class Recursion
+    {
+        public static int GCD(int x, int y)
+        {
+            if (y == 0)
+            {
+                return x;
+            }
+
+            return x > y ? GCD(x % y, y) : GCD(x, y % x);
+        }
+
+        public static int Fibonacci(int x)
+        {
+            // Fill Up this function body
+            // You can add another function as well, if required///
+            return FibonacciRecursion(1, 0, 3, x);
+        }
+
+        private static int FibonacciRecursion(int last, int secondLast, int n, int terminate)
+        {
+            if (n == terminate)
+            {
+                return last + secondLast;
+            }
+
+            return FibonacciRecursion(last + secondLast, last, n + 1, terminate);
+        }
+
+        public static List<string> PascalTriangle(int row)
+        {
+            List<string> result = new List<string>();
+
+            for (int rowIndex = 0; rowIndex < row; rowIndex++)
+            {
+                result.Add(GetPascalTriangleRow(rowIndex + 1));
+            }
+
+            return result;
+        }
+
+        private static string GetPascalTriangleRow(int row)
+        {
+            return string.Empty;
+        }
+    }
+}
